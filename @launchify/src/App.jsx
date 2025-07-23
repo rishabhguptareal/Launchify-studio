ec3a3f6cc5ab5e711f68a88e8a8fefe50a3f3617
Rishabh Gupta <rg6367229@gmail.com>
2025-07-23 16:00:36 +0530

    Fix App.jsx JSX and style

--- @launchify/src/App.jsx
+++ @launchify/src/App.jsx
@@ -1,5 +1,16 @@
 import React from 'react'
+import { motion } from 'framer-motion';
 function App() {
-  
+  return (
+    <span
+      data-text-fill="true"
+      className="framer-text"
+      style={{
+        backgroundImage: "radial-gradient(43% 228.297% at 50% 60.6452%, rgba(0, 0, 0, 0.8) 0%, rgb(94, 94, 94) 100%)"
+      }}
+    >
+      Premium Websites for SaaS & Startups
+    </span>
+  );
 }
 export default App
